32858478a3eef0ac4f47c6dd7736360a9decb46a
wandji69 <bobgentle67@gmail.com>
2018-09-01 01:16:09 +0100

    --allow-empty-message

--- Chapter10/CarbonFootprint/CarbonFootprint.java
+++
@@ -1,8 +0,0 @@
-/*
- * Exercise 10.17 : CarbonFootprint Interface Declaration
- */
-
- public interface CarbonFootprint
- {
-     double getCarbonFootprint();
- }
--- Chapter10/Payroll/Payable.java
+++
@@ -1,9 +0,0 @@
-/*/
- * Figure 10.11 : Payable.java
- * Payable interface declaration
- */
-
- public interface Payable
- {
-     double earnings(); // calculate payment, ( no implementation...although default methods in 8 can implement )
- }
