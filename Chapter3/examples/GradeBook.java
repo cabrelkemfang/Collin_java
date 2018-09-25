9ea115513105095ca6f7e146ed47ec2438e2d7b9
Wandji69 <collinschuwa@gmail.com>
2018-09-25 22:40:02 +0100

    Working with classes. Objects Methods and Strings

---
+++ Chapter3/examples/GradeBook.java
@@ -0,0 +1,13 @@
+// Fig. 3.1: GradeBook.java
+// Class declaration with one method
+// Page 73
+//
+
+
+public class GradeBook
+{
+   public void displayMessage()
+   {
+      System.out.println("Welcome to the Grade Book!");
+   }
+}
---
+++ Chapter3/examples/GradeBookTest.java
@@ -0,0 +1,13 @@
+// Fig. 3.2: GradeBookTest.java
+// Creating a GradeBook object and calling its displayMessage method.
+// Page 74
+//
+public class GradeBookTest
+{
+   public static void main( String[] args )
+   {
+      GradeBook myGradeBook = new GradeBook();
+
+      myGradeBook.displayMessage();
+   }
+}
